941b6c217b2a878b9b4011a1114f5b5d1c0dc34b
Rishita2009 <93776061+Rishita2009@users.noreply.github.com>
2022-04-26 16:42:14 +0530

    Add files via upload

---
+++ a main.js
@@ -0,0 +1,13 @@
+function maths_quiz()
+{
+    window.location="index.html"
+}
+function Golf_Game()
+{
+    window.location="kolf game index.html"
+}
+function Guess_word()
+{
+
+    window.location="word.html"
+}
